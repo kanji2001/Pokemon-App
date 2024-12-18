c6ce42504abca4ff068607f7a187d5ea6a858e5b
morinikunj <morinikunj123@gmail.com>
2024-12-18 17:56:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import Pokemon from './Components/Pokemon/Pokemon';
-// import LoginForm from './Components/Hooks/LoginForm';
-// import ToDo from './Components/ToDo/ToDo';
-// import ToggleSwitch from './Components/MiniProject/ToggleSwitch';
+
 
 function App() {
   return (
    <>
-      {/* <ToggleSwitch/> */}
-      {/* <ToDo/> */}
-      {/* <LoginForm/> */}
+  
       <Pokemon/>
    </>
   );
